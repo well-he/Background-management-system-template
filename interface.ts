93257079672dd373869fcc725ecabb9615b61f0a
well-he <well_v@foxmail.com>
2023-08-20 23:53:35 +0800

    feat: recode

--- interface.ts
+++
@@ -1,7 +0,0 @@
-interface storage {
-    setItem(key: string, val: any): void;
-    getItem(key: string): object;
-    getStorage(): object;
-    clearItem(key: string): void;
-    clearAll(): void;
-}
